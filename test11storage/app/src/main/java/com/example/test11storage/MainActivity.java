d348d4475248c29f03378a97e0245082abcdae6e
zhangweixiao <zhangweixiao@live.com>
2021-04-25 14:15:13 +0800

    测试安卓11的访问所有文件的权限

---
+++ test11storage/app/src/main/java/com/example/test11storage/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.test11storage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Environment.isExternalStorageManager()) {
+            //todo when permission is granted
+        } else {
+            //request for the permission
+            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+            Uri uri = Uri.fromParts("package", getPackageName(), null);
+            intent.setData(uri);
+            startActivity(intent);
+        }
+    }
+}
